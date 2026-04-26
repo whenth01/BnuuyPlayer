37092f0c79243ca4d66ab280087031a0f2a2bd0f
whenth01 <dodifoxy231@gmail.com>
2026-04-26 21:00:24 +0300

    BnuuyPlayer
    Ultralight music player.
    
    V2features:
    
    1) MPV Music player(ran with subprocess)
    
    2) Shuffle toggle(saved between sessions)
    
    3) Customizable keybinds(by autogenerated .conf file)
    
    4) Manual folder adder, automatic folder creation, folder searcher, folder deletion, and display names for folders(folders are treated as playlists)
    
    5) Persistence (via autogenerated .json), saving
    
      5.1) whether to run setup or not
    
      5.2) your folders
    
      5.3) shuffle toggle
    
      5.4) hint toggle
    
    6) Default keybind ref sheet.
    
    7) A settings menu.
    
    8) some bnuuy themed ascii art!
    
    Future additions
    
    1) Youtube streaming/downloading with YTDLP
    
    2) Individual song picker
    
    Both are in development.

---
+++ BnuuyPlayer_v2.py
@@ -0,0 +1,644 @@
+import os, subprocess, sys, shutil, pathlib, json
+
+
+
+no_hint = [False]
+initialized = [False]
+shuffl = [False, "placeholder"]
+
+bnuy_path = os.path.dirname(__file__)
+
+bnuybinds = """WHEEL_UP      add volume  2
+WHEEL_DOWN    add volume -2
+WHEEL_LEFT    seek -10
+WHEEL_RIGHT   seek  10
++ seek  5
+- seek -5
+s seek  30
+b seek -30
+[ multiply speed 0.9
+] multiply speed 1.1
+{ multiply speed 0.5
+} multiply speed 2.0
+BS set speed 1
+q quit
+Q quit-watch-later
+p cycle pause
+SPACE cycle pause
+> playlist-next
+< playlist-prev
+P show-progress
+i script-binding stats/display-stats-toggle
+G add sub-scale  0.1
+F add sub-scale -0.1
+9 add volume 2
+0 add volume -2
+m cycle mute
+l cycle-values loop-file inf no
+"""
+
+try: 
+  directory = os.path.join(bnuy_path, "bnuybinds.conf")
+  is_conf = os.path.exists(directory)
+  if not is_conf:
+    raise FileNotFoundError 
+
+except(FileNotFoundError):
+  with open(directory, "w") as f:
+    f.write(bnuybinds)
+
+def initializer(initialized):
+  if not initialized[0]:
+    initialized[0] = True
+
+
+#### KEYBINDED MENU ####
+
+def binding_menu():
+  print(f"""\nKeybindings;
+q) Quit
+Q) Quit, but saves position
+l) Loop current song
+m) Mute
+p / space ) Pause
+P) show progress
+i) Info
+< / >) Go backward/forward in the playlist
+[ / ]) -10% and +10% playback speed
+{{ / }}) Half/double playback speed
+Backspace) Reset playback speed to normal
+9 / 0) Vol +2 / -2
++ / -) skip 5s / back 5s
+s / b) skip 30s / back 30s
+G / F) Increase / decrease subtitle size
+""")
+
+def adder_menu():
+  choice = int(input(f"""1) Specify the path to your own folder.
+
+2) Allow BnuuyPlayer to make a folder automatically.
+
+3) Allow BnuuyPlayer to search for a specified folder.
+(BnuuyPlayer can only search within the folder it's in.)
+
+4) Skip.
+(Warning, This is intended for testing, unexpected behavior may occur.)
+
+
+>>> """))
+  os.system('cls' if os.name == 'nt' else 'clear')
+  return choice
+
+############# MAIN #############
+
+#### MUSIC PLAYER ####
+
+def audio_funct(directory):
+  os.system('cls' if os.name == 'nt' else 'clear')
+
+  result = playlist_picker(song_paths, bnuy_path, shuffl, directory)
+  try:
+    _, _, _, path, player = result
+    binding_menu()
+    print("")
+    subprocess.run(player)
+  except(ValueError, KeyError):
+    pass
+
+
+#### PLAYLIST PICKER ####
+def playlist_picker(song_paths, bnuy_path, shuffl, directory):
+  os.system('cls' if os.name == 'nt' else 'clear')
+  while True:
+    try:
+      counter = 0
+      for num, (name, _, _) in song_paths.items():
+        print(f"{num}) {name}")
+        counter += 1
+
+      choice = int(input("""0) back
+>>> """))
+
+      if choice == 0:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        return choice, counter
+
+      (name, path, function) = song_paths[choice]
+      if not shuffl[0]:
+        player = ["mpv", path, f"--input-conf={directory}", "--profile=fast", "--no-video"]
+      else:
+        player = ["mpv", path, f"--input-conf={directory}", "--profile=fast", "--no-video", "--shuffle"]
+
+      return choice, name, function, path, player
+    except(KeyError, ValueError):
+      print("Invalid input.")
+      continue
+
+
+
+#### PLAYLISTS ####
+
+song_paths = {}
+bulk_save = {}
+
+#### PERSISTENT HIST CREATOR ####
+
+try:
+
+  hist_path = os.path.join(bnuy_path, "BnuyPlayerHist.json")
+  with open(hist_path, 'r') as f:
+    bulk_save = json.load(f)
+    song_paths = bulk_save["0"]
+    initialized = bulk_save["1"]
+    no_hint = bulk_save["2"]
+    shuffl = bulk_save["3"]
+ 
+    if len(song_paths) < 1:
+      pass
+
+    tmp_handler = {}
+    for num, (name, combined) in song_paths.items():
+      tmp_handler[num] = (name, combined, audio_funct)
+
+    song_paths = tmp_handler
+    res = {i: v for i, v in enumerate(song_paths.values(),start=1)}
+    song_paths.clear()
+    song_paths.update(res)
+
+
+
+except(FileNotFoundError,AttributeError,KeyError,json.JSONDecodeError):
+  with open(hist_path, "w") as f:
+    json.dump(bulk_save, f, indent=2) 
+
+############# MAIN FOLDER/SETUP AREA #############
+
+
+#### HISTORY ADDER ####
+
+def saver(song_paths, initialized, shuffl, no_hint, bulk_save, hist_path):
+  tmp_handler = {}
+
+  for num, (name, combined, audio_funct) in song_paths.items():
+    tmp_handler[num] = (name, combined)
+
+
+  song_paths = tmp_handler
+
+  bulk_save = {}
+
+  bulk_save[0] = song_paths
+  bulk_save[1] = initialized
+  bulk_save[2] = no_hint
+  bulk_save[3] = shuffl
+
+  with open(hist_path, 'w') as f:
+    json.dump(bulk_save, f, indent=2)
+
+
+#### EXIT FUNCT ####
+
+def exity(_):
+  sys.exit()
+
+
+#### PLAYLIST/PATH ADDER ####
+
+def path_adder(song_paths, initialized, bnuy_path, bulk_save, no_hint, hist_path):
+  os.system('cls' if os.name == 'nt' else 'clear')
+  while True:
+    try:
+      print("""Hint: Valid file paths are:
+
+MacOS: /users/<your_username>/...
+Linux: /home/<your_username>/...
+Android: /storage/emulated/0/...
+Windows: C:\\users\\<your_username>\\...
+
+These are how your device sees your folders/files.
+A folder named "Synth" on android in the Home dir would be 
+/storage/emulated/0/synth\n""")
+      path_input = input("""B) Back
+Please input the path to the folder.
+>>> """)
+      is_playlist_path = os.path.isdir(path_input)
+
+      if path_input == "B":
+        break
+
+      if not is_playlist_path:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("\nDirectory dosen't exist, or you made a typo.")
+        continue
+
+      else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        while True:
+          name_choice = input("""Would you like to use the folder name, or create a display name?
+1) Create a display name(Only affects how Bnuuyplayer shows you the playlist!)
+2) Use the folder name
+
+>>> """)
+          if name_choice == "1": 
+            playlist_name = input("""\nEnter the display name.
+>>> """)
+            break
+
+          elif name_choice == "2": 
+            playlist_name = os.path.basename(path_input)
+            break
+
+          else:
+            print("Invalid choice!")
+            continue
+
+        song_path_len = len(song_paths)+1
+        song_paths[song_path_len] = (playlist_name, path_input, audio_funct)
+        saver(song_paths, initialized, shuffl, no_hint, bulk_save, hist_path)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Playlist successfully added!")
+
+        initializer(initialized)
+      
+        path_input = input("""Would you like to add one more or continue?
+c) Continue to BnuuyPlayer.
+a) Add one more path.
+>>> """).lower()
+
+      if path_input == "c": break
+      elif path_input == "a": continue
+      else: raise ValueError
+
+    except(ValueError):
+      print("Invalid input.\n")
+      continue
+
+#### ADD/CREATE FOLDER ####
+
+def folder_maker(song_paths, initialized, bnuy_path, bulk_save, no_hint, hist_path):
+  os.system('cls' if os.name == 'nt' else 'clear')
+  while True:
+    try:
+      folder_name = input("""Continue to let BnuuyPlayer to make a folder.
+1) Continue
+2) Back
+>>> """)
+      if folder_name == "1":
+        os.system('cls' if os.name == 'nt' else 'clear')
+        folder_name = input("""What would you like to name the playlist?
+>>> """)
+
+        song_path = os.path.join(bnuy_path, folder_name)
+        os.makedirs(song_path)
+
+        num = len(song_paths)+1
+
+        song_paths[num] = (folder_name, song_path, audio_funct)
+        saver(song_paths, initialized, shuffl, no_hint, bulk_save, hist_path)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"""Successfully created.
+Path to the new playlist folder) {song_path}
+
+You can add any song to the newly created playlist.""")
+
+        initializer(initialized)
+
+        break
+
+      elif folder_name == "2":
+        break
+
+      else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        raise ValueError
+
+    except(FileExistsError):
+      print("\nFolder already exists.")
+      continue
+
+    except(OSError):
+      print(f"""\nUnknown Error. You likely use an invalid character/name for the folder name.
+
+Invalid character/name list:
+Windows: 
+< > : - " / \\ | ? *
+
+CON, PRN, AUX, 
+NUL COM1, COM2, COM3, COM4, COM5, COM6, COM7, COM8, COM9 
+LPT1, LPT2, LPT3, LPT4, LPT5, LPT6, LPT7, LPT8, LPT9
+
+0-31 (ASCII control characters)
+
+Names also cannot end in a dot or space.
+
+Linux:
+0 (NULL byte)
+/
+. (special name referring to current directory) 
+.. (special name referring to parent directory)
+
+macOS:
+:
+/
+
+Android:
+< > : - " / \\ | ? *
+
+\\n
+0-31 (ASCII control characters)""")
+      continue
+    
+    except(ValueError):
+      print("\nInvalid input.")
+      continue
+
+
+#### DIRECTORY SEARCHER ####
+
+def song_searcher(song_paths, initialized, bnuy_path, bulk_save, no_hint, hist_path):
+  os.system('cls' if os.name == 'nt' else 'clear')
+  song_path_len = 0
+  song_path_len += len(song_paths)
+  while True:
+    name = input("""Please enter the folder name you'd like BnuuyPlayer find.
+B) return
+
+>>> """)
+
+    if name == "B": break
+
+    results = {}
+    res_len = len(results)
+
+    try:
+      for root, dirs, files in os.walk(bnuy_path): 
+        if name in dirs:
+          res_len += 1
+
+          combined = os.path.join(root, name)          
+          results[res_len] = (name, combined, audio_funct)
+
+      if len(results) > 1:
+        print("\nMultiple folders found!")
+
+        while True:
+          for key, (name, root, _) in results.items():
+            print(f"{key}) found at: {root}")
+          choice = int(input("""Which one is correct? If all are, enter "0"
+>>> """))
+
+          if choice > key or choice < 0:
+            print("Invalid option.")
+            continue
+          else: break
+
+        if choice == 0:
+          for key, (name, root, _) in results.items():
+            song_path_len += 1
+            song_paths[song_path_len] = (name, root, audio_funct)
+            combined = root
+
+        else:
+          song_path_len += 1
+          (name, root, _) = results[choice]
+          song_paths[song_path_len] = (name, root, audio_funct)
+          combined = root
+
+      elif len(results) == 1:
+        song_path_len += 1
+        song_paths[song_path_len] = (name, combined, audio_funct)
+
+      else:
+        raise UnboundLocalError
+
+      os.system('cls' if os.name == 'nt' else 'clear')
+      choice = int(input(f"""Successfully found at {combined}\n
+1) Return to BnuuyPlayer
+2) Add another folder.
+>>> """))
+      saver(song_paths, initialized, shuffl, no_hint, bulk_save, hist_path)
+      initializer(initialized)
+
+      if choice == 1: break
+      elif choice == 2: continue
+      else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        raise ValueError
+
+    except(UnboundLocalError):
+      print("\nFolder not found.")
+      continue
+    except(ValueError):
+      print("\nInvalid input.")
+      continue
+
+#### ADDER DICT ####
+
+adders = {
+1: path_adder,
+2: folder_maker,
+3: song_searcher,
+4: "Skip."
+}
+
+
+#### SETTINGS / NON-MUSIC ####
+def settings(shuffl, song_paths, adders, bulk_save, initialized, no_hint, hist_path):
+  while True:
+    try:
+      choice = int(input("""\n1) Toggle shuffle. (This is saved between sessions!)
+2) Delete a playlist.
+3) Add a playlist.
+0) Return.
+
+WIP
+>>> """))
+      os.system('cls' if os.name == 'nt' else 'clear')
+
+
+      if choice == 0:
+        saver(song_paths, initialized, shuffl, no_hint, bulk_save, hist_path)
+        break
+
+      if choice == 1:
+        if not shuffl[0]:
+          shuffl[0] = True
+          shuffl[1] = "activated"
+        else: 
+          shuffl[0] = False
+          shuffl[1] = "deactivated"
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"Shuffle has been {shuffl[1]} \n")
+        continue
+
+      elif choice == 2:
+        for num, (name, _, _) in song_paths.items():
+          print(f"{num}) {name}")
+        del_choice = int(input("""Which would you like to delete? 
+(This only deletes the playlist from BnuuyPlayer!)
+0) return
+
+>>> """))
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if del_choice == 0:
+          continue
+        else:
+          del song_paths[del_choice]
+
+          res = {i: v for i, v in enumerate(song_paths.values(),start=1)}
+          song_paths.clear()
+          song_paths.update(res)
+ 
+          print(f"Successfully deleted!\n")
+          saver(song_paths, initialized, shuffl, no_hint, bulk_save, hist_path)
+
+        continue
+
+      elif choice == 3:
+        choice = adder_menu()
+        if choice == 4:
+          continue
+
+        funct = adders[choice]
+        funct(song_paths, initialized, bnuy_path, bulk_save, no_hint, hist_path)
+
+    except(ValueError, KeyError):
+      os.system('cls' if os.name == 'nt' else 'clear')
+      print("Invalid input.\n")
+      
+
+#### OPERATIONS ####
+
+main_operations = {
+"1": ("Playlists", "Your library, this is where your songs and playlists live.", audio_funct),
+"2": ("Keybinds", "Music player keybinds.", binding_menu),
+"3": ("Settings", "Your settings, you can toggle shuffle, add/remove songs, and do more here.", settings),
+"e": ("Exit", "Closes the audio player", exity)
+}
+
+#### INITIAL SETUP ####
+
+def file_setup(initialized, song_paths, adders):
+  os.system('cls' if os.name == 'nt' else 'clear')
+  if not initialized[0]:
+    while True:
+      try:
+        print(f"""
+⠀⠀⠀⡤⣤⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡶⠶⡶⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣸⠉⠉⠙⠶⣶⣄⠀⠀⠀⠀⠀⠀⣾⠉⣀⣀⠉⠻⠶⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣿⠀⡖⢶⣀⡉⠉⣷⠀⠀⠀⠀⠀⣿⠀⡇⠉⢶⣄⠤⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀
+⠀⠀⣿⣀⢇⠈⠋⣷⠉⠉⣿⠀⠀⠀⠀⣿⠀⡇⠀⠀⢿⣀⡷⠿⣀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣿⠿⠽⠉⠉⠉⠭⠿⣿⣿⣀⠀⠀⠀
+⠀⠀⠀⣿⠀⡇⠈⠙⣷⠠⠿⣀⡀⠀⠀⣿⠀⡇⠀⠀⠀⣿⡇⢀⣿⠃⠀⠀⠀⠀⢀⣀⣾⢿⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠿⣷⣀⡀
+⠀⠀⠀⠉⣾⠛⡀⠀⣿⠀⢘⣿⡇⠀⠀⠿⢀⠉⡂⠀⠀⣿⡆⢸⣿⣷⠶⠶⠶⠶⠿⠿⣿⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⣿⡇
+⠀⠀⠀⠀⠙⣷⠉⢶⣿⣶⠻⣿⣇⣀⣀⣬⣶⡆⠀⠀⣶⣿⡅⣙⣿⠋⠀⠀⠀⠀⠀⠀⠉⠙⣷⣶⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⣿⣶
+⠀⠀⠀⠀⠀⢩⣶⣿⣿⠿⠶⠉⠁⠉⠯⣿⣿⠇⣄⣶⣿⢿⢷⣿⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⠉⠯⣷⣆⣐⠀⠀⠀⠀⠀⠀⠩⣿
+⠀⠀⠀⠀⢀⣘⠿⠍⠉⠀⠀⠀⠀⠀⠀⠉⠿⠆⣿⣿⣿⠮⣿⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠧⠀⠉⢿⣿⣀⠀⠀⠀⠀⠀⣸⣿
+⠀⠀⠀⠀⣟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠯⡉⠀⠉⠸⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⣿⠶⣀⠀⠀⣀⣿⡏
+⠀⠀⠀⣶⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣷⣿⡿⠉⠁
+⠀⠀⣾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⡿⠋⠀⠀⠀
+⠀⠀⣿⡷⢶⣄⠀⠀⠀⠀⢀⣠⡶⠶⠶⠶⢀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡏⠀⠀⠀⠀⠀
+⠀⠀⣿⠱⣀⠉⣀⣀⣀⡀⠀⠉⠃⠀⠀⠀⠉⣀⣀⣖⡷⠶⠷⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀
+⣰⠾⠿⣄⠈⠉⠗⠿⠟⠃⠀⠀⠀⠀⠀⠒⣒⡿⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⡇⠀⠀⠀⠀⠀
+⣿⠀⠀⠉⠷⣤⣤⣀⠀⠀⠀⠀⠀⠀⣠⣤⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⣄⣀⣾⣿⠉⠁⠀⠀⠀⠀⠀
+⠿⠷⠶⠶⠶⠿⠶⠿⠿⠶⠶⠶⠾⠾⠿⠿⠿⠷⠾⠶⠷⠶⠾⠾⠾⠷⠷⠶⠾⠷⠾⠷⠶⠶⠾⠷⠾⠿⠿⠿⠋⠀⠀⠀⠀⠀⠀⠀
+ ________________________________________________________________________
+|Welcome to BnuuyPlayer!                                                 |
+|________________________________________________________________________|
+
+To use BnuuyPlayer, there must first be a valid song folder/playlist.""")
+        choice = adder_menu()
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        if choice in range(1, 4): 
+          funct = adders[choice]
+          funct(song_paths, initialized, bnuy_path, bulk_save, no_hint, hist_path)
+          break
+        elif choice == 4:
+          initializer(initialized)
+          break
+
+        else: raise ValueError
+        
+
+      except(ValueError):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("\nInvalid Input.")
+        continue
+          
+        
+
+
+#### MAIN MENU ####
+
+def main_menu(main_operations, song_paths, initialized, no_hint, hist_path, bulk_save, shuffl):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("""
+⠀⠀⣠⡶⢶⣦⠀⠀⠀⣠⡶⢶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢰⡟⠀⠀⢹⣧⠀⣸⠏⠀⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣿⠁⠀⠀⠀⢿⣴⡿⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣿⠀⠀⠀⠀⢸⣿⠇⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢿⡆⠀⠀⠀⠈⣿⠀⠀⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀
+⠀⢸⣷⠀⠀⠀⠀⠀⠀⠀⠀⢰⣟⠀⠀⠀⣀⣀⣀⣀⣀⣾⠋⠉⠹⣇⠀⠀⠀⠀
+⠀⣰⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣧⣶⠞⠋⠉⠀⠈⠉⠃⠀⠀⢠⡟⠀⠀⠀⠀
+⢠⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣦⡀⠀⠀
+⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⣄⠀
+⢿⡇⠸⣿⠀⠀⠀⠀⠀⠀⣴⣆⠀⢸⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆
+⠸⣧⡀⠀⠀⢀⣶⣶⡆⠀⠈⠁⣰⡟⠁⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⣸⡇
+⠀⠙⠻⣦⣄⣀⣀⣈⣁⣀⣤⠾⠋⠀⠀⠀⠀⠀⣀⣠⣴⡶⢿⡿⠿⠶⣶⠶⠟⠀
+⠀⠀⢠⡟⠉⢙⣿⠛⠋⠉⠁⠀⠀⣀⣠⣴⠶⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠘⢿⣤⣘⣿⡀⠀⠀⢀⣴⡿⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠉⠙⠛⠻⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ ________________________________________________________________________
+|Welcome back to BnuuyPlayer!                                            |
+|________________________________________________________________________|
+⠀⠀⠀⠀
+""")
+
+    while True:
+      try:
+        for key, (name, _, _) in main_operations.items():
+          print(f"{key}) {name}")
+
+        if not no_hint[0]:
+          print("Hint: enter h / H for extra information, or 0 to toggle this message.")
+        choice = input(">>> ").lower()
+
+        if choice == "h":
+          os.system('cls' if os.name == 'nt' else 'clear')
+          for num, (name, hint, _) in main_operations.items():
+            print(f"""{num}) {name}
+Info: {hint}
+""")
+          continue
+
+        elif choice == "0":
+          os.system('cls' if os.name == 'nt' else 'clear')
+
+          if no_hint[0]:
+            no_hint[0] = False
+          else:
+            no_hint[0] = True
+
+          saver(song_paths, initialized, shuffl, no_hint, bulk_save, hist_path)
+          print("Successfully toggled.\n")
+          continue
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        (name, _, function) = main_operations[choice]
+        if name == "Settings":
+          function(shuffl, song_paths, adders, bulk_save, initialized, no_hint, hist_path)
+        elif name == "Keybinds":
+          function()
+        else: function(directory)
+
+      except(KeyError):
+        print("\nInvalid input.")
+        continue
+
+while True:
+  if not initialized[0]:
+    file_setup(initialized, song_paths, adders)
+  else:
+    main_menu(main_operations, song_paths, initialized, no_hint, hist_path, bulk_save, shuffl)
